acf5945f36d35348647ce0d9721f7e11dd102f3e
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2025-02-25 17:56:56 -0600

    Created manifest

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Moto-loan',
+    'version': '18.0.0.0.1',
+    'category': 'Kawiil/Custom Modules',
+    'license': 'OPL-1',
+    'summary': 'Streamlines the loan application process for dealerships. Alejandro',
+    'description': 'A module to handle motorcycle loan applications, tracking, and management',
+    'author': 'Odoo-sh-account',
+    'website': 'https://github.com/odoo-sh-account/final_motor_deleon',
+    'support': 'support@deleonlangure.com',
+    'sequence': 10,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'depends': ['base'],
+    'data': [
+
+    ],
+}
