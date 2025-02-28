3e731517e777cf1dcb4f5ac30d849016908d351f
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2025-02-28 08:57:22 -0600

    Added sale_management dependency

--- final_motor_deleon/__manifest__.py
+++ final_motor_deleon/__manifest__.py
@@ -12,7 +12,7 @@
     'installable': True,
     'application': True,
     'auto_install': False,
-    'depends': ['base', 'mail', 'sale', 'product'],
+    'depends': ['base', 'mail', 'sale', 'product', 'sale_management'],
     'data': [
         'security/financing_security.xml',
         'security/motorcycle_financing_groups.xml',
