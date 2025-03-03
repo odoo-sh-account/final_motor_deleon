54f57da4997b6c03583f19942487c296c03972a4
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2025-03-03 10:08:37 -0600

    added spreadsheet to dependencies

--- final_motor_deleon/__manifest__.py
+++ final_motor_deleon/__manifest__.py
@@ -12,7 +12,7 @@
     'installable': True,
     'application': True,
     'auto_install': False,
-    'depends': ['base', 'mail', 'sale', 'product', 'sale_management'],
+    'depends': ['base', 'mail', 'sale', 'product', 'sale_management', 'spreadsheet'],
     'data': [
         'security/financing_security.xml',
         'security/motorcycle_financing_groups.xml',
