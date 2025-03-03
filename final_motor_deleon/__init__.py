4d88ff34d727f90c9add4936c7f58e0e6dbcdbee
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2025-03-03 10:50:14 -0600

    added demo data function call

--- final_motor_deleon/__init__.py
+++ final_motor_deleon/__init__.py
@@ -4,17 +4,25 @@ from odoo import api, SUPERUSER_ID
 
 
 # In response of the error during uninstall: KeyError: 'loan.application'
+# In __init__.py
 def uninstall_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     
-    # Clean up computed fields before model removal
+    # Proper model cleanup
     Model = env['ir.model'].search([('model', '=', 'loan.application')])
     if Model:
-        Model.write({'state': 'manual'})  # Disable model tracking
+        # First remove fields to prevent ORM errors
+        Field = env['ir.model.fields'].search([
+            ('model_id', '=', Model.id)
+        ])
+        Field.unlink()
         
-    # Remove any leftover fields
-    Field = env['ir.model.fields'].search([
+        # Then remove the model itself
+        Model.unlink()
+    
+    # Clean up sale.order fields
+    SaleFields = env['ir.model.fields'].search([
         ('model', '=', 'sale.order'),
         ('name', 'in', ['spreadsheet_template_id', 'spreadsheet_id'])
     ])
-    Field.unlink()
+    SaleFields.unlink()
